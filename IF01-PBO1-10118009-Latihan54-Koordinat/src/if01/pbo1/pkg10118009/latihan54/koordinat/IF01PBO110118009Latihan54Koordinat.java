483ca7f97164ac2cc07732e6f207ed5ce5d867af
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-11-02 16:32:15 +0700

    Add files via upload

---
+++ IF01-PBO1-10118009-Latihan54-Koordinat/src/if01/pbo1/pkg10118009/latihan54/koordinat/IF01PBO110118009Latihan54Koordinat.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pbo1.pkg10118009.latihan54.koordinat;
+
+/**
+ * Nama = Tassyakur Pasya
+ * Kelas = IF01
+ * NIM = 10118009
+ * Deskripsi Program = Koordinat
+ * @author LENOVO
+ */
+public class IF01PBO110118009Latihan54Koordinat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WarnaKoordinat k = new WarnaKoordinat(10, 4, "jingga");
+        k.setNamaWarna(k.getNamaWarna());
+        System.out.println("Koordinat sumbu x :"+k.getX()+", y :"+k.getY());
+        
+        
+      
+     
+        
+    }
+    
+}
